8fe9ed7070787a88cbbee4d39f7bdb9926779722
ArtemMasharipov <112805569+ArtemMasharipov@users.noreply.github.com>
2023-10-28 15:31:39 +0300

    Add files via upload

---
+++ lesson_5/lesson_5_github/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="container">
+		<div class="header">
+			<h1>ДЗ. Урок №5. Практика.</h1>
+		</div>
+		<product-cards-panel :cards-list-data="productCardsSources" />
+		<notebook-cards-panel :notebook-cards-list-data="notebookCardsSources"/>
+	</div>
+</template>
+
+<script>
+import ProductCardsPanel from './hw_lesson_5_components/task_2_components/ProductCardsPanel.vue';
+import { productCardsSources } from './constants/task_2_constants/product_card_data';
+
+import NotebookCardsPanel from "./hw_lesson_5_components/task_3_components/NotebookCardsPanel.vue";
+import { notebookCardsSources } from "./constants/task_3_constants/notebook_cards_data";
+export default {
+	name: "App",
+	components: {
+		ProductCardsPanel,
+		NotebookCardsPanel
+	},
+	data() {
+		return {
+			productCardsSources,
+			notebookCardsSources
+		}
+	},
+};
+</script>
+
+<style>
+@import url(./assets/reset.css);
+
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	margin-top: 80px;
+}
+
+.header {
+	position: fixed;
+	width: 100%;
+	left: 0;
+	top: 0;
+	padding: 15px;
+	background: rgb(2, 0, 36);
+	background: linear-gradient(90deg,
+			rgba(2, 0, 36, 1) 0%,
+			rgb(167, 171, 168) 35%,
+			rgb(204, 218, 221) 100%);
+	opacity: 0.8;
+	z-index: 10;
+	text-align: center;
+	text-transform: uppercase;
+	font-size: 30px;
+}
+</style>
---
+++ lesson_5/lesson_5_github/src/hw_lesson_5_components/task_3_components/NotebookCardsPanel.vue
@@ -0,0 +1,44 @@
+<template>
+	<h2>Задача 3</h2>
+	<div class="notebook-cards-panel-container">
+		<notebook-cards-item v-for="card in notebookCardsListData" :key="card.id" :notebook-cards-item-data="card"/>
+	</div>
+</template>
+
+<script>
+import NotebookCardsItem from './NotebookCardsItem.vue';
+export default {
+	name: 'NotebookCardsPanel',
+	components: {
+		NotebookCardsItem,
+	},
+	props: {
+		notebookCardsListData: {
+			type: Array,
+			default: () => [],
+		},
+	},
+
+}
+</script>
+
+<style lang="css" scoped>
+h2 {
+	margin-top: 100px;
+	text-align: center;
+	font-weight: bold;
+	font-size: 20px;
+}
+
+.notebook-cards-panel-container {
+	border-radius: 5px;
+	margin: 10px;
+	margin-left: 15%;
+	margin-right: 15%;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: center;
+	border: 2px solid black;
+}
+</style>
